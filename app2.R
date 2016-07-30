8e13480cddbcdd4ae93b770ccfa3927f5e4bcd4d
mihiriyer <knowledge@jangal.info>
2016-07-30 19:52:23 -0400

    working copy of leafet GDX map (FY15).

---
+++ app2.R
@@ -0,0 +1,93 @@
+#load the libraries
+library(shiny)
+library(dplyr)
+library(sp)
+library(rgeos)
+library(rgdal)
+library(maptools)
+library(scales)
+library(leaflet)
+
+###MAP Setup
+#read census shape file
+us.map <- readRDS("us.map.Rda")
+
+# Remove Virgin Islands (78), American Samoa (60) Mariana Islands (69), Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74), Alaska (02), Guam (66), Hawaii (15), Puerto Rico (72) == , "02", "72", "66", "15"
+us.map <- us.map[!us.map$STATEFP %in% c("78", "60", "69", "64", "68", "70", "74"),]
+
+##DATA Setup
+#load the R data object of gdxcty - the FY10-15 consolidated, cleanedup, and linked to FIPS code
+gdxcty15 <- readRDS("gdxcty15.Rda") 
+
+#Concatenate StateFP and CountyFP for a unique ID that can be matched with GEOID in us.map
+gdxcty15 <- mutate(gdxcty15, FIPS=paste(StateFP, CountyFP, sep="")) %>% 
+  select(c(18, 8:12,14:17))
+
+#As of FY15 - Shannon County SD (Fips: 46-113) is now Ogmerge(us.map, county_dat, by.x="GEOID", by.y="FIPS")alala-Lakota County (Fips: 46-102)
+gdxcty15$FIPS[gdxcty15$FIPS == "46113"] <- "46102"
+
+#create a vector with col names to allow selecting a variable to view
+gdxvars <- names(gdxcty15[2:10])
+
+us.map <- merge(us.map, gdxcty15, by.x="GEOID", by.y="FIPS")
+
+#create a color palette
+pal <- colorQuantile("YlGnBu", NULL, n = 9)
+
+#User Interface
+ui <- navbarPage("GDX 2015", theme = "bootstrap.css",
+                 tabPanel("My Application",
+                          div(class="outer",
+                              tags$head(
+                                # Include our custom CSS
+                                includeCSS("styles.css"),
+                                includeScript("gomap.js")
+                              ),
+                              #display the leaflet map
+                              leafletOutput("mymap", width="100%", height="100%"),
+                              absolutePanel(id = "controls",
+                                            class = "panel panel-default",
+                                            fixed = TRUE,
+                                            draggable = TRUE,
+                                            top = "auto",
+                                            left = 20,
+                                            right = "auto",
+                                            bottom = "700",
+                                            width = 330,
+                                            height = "auto",
+                                            
+                                            #header title
+                                            h2("GDX explorer"),
+                                            
+                                            #drop-down to pick a gdx variable
+                                            selectInput(inputId = "gdxvar",
+                                                        label = "Select a variable to map",
+                                                        choices = gdxvars,
+                                                        selected = gdxvars[1]),
+                                            
+                                            textOutput("Mihir")
+                              )
+                          )
+                          
+                 ),
+                 tabPanel("Tab 01"),
+                 tabPanel("Tab 02")
+)
+
+
+#SERVER logic
+server <- function(input, output){
+
+  
+  output$mymap <- renderLeaflet({
+    leaflet(data = us.map)  %>%
+      addTiles() %>%
+      addPolygons(fillColor = ~pal(as.numeric(us.map@data[input$gdxvar][,1])), 
+                  fillOpacity = 0.8, 
+                  color = "#BDBDC3", 
+                  weight = 1) %>% 
+      setView(lng = -93.85, lat = 37.45, zoom = 4)
+  })
+}
+
+shinyApp(ui = ui, server = server)
