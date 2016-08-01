4cf4e2a432646d868b5747496dfcb2d2da3dd039
mihiriyer <knowledge@jangal.info>
2016-08-01 17:15:37 -0400

    added About page and a lil' plot under the selector

---
+++ about.md
@@ -0,0 +1,68 @@
+### Introduction
+
+Each year the [National Center for Veterans Analysis and Statistics (NCVAS)](http://www.va.gov/vetdata/) publishes a report on the Geographic Distribution of VA Expenditures (GDX). These reports provide a break-out of expenditures for key VA programs by State, County, and Congressional District and go as far back as 1996. You can view the [GDX reports and others on the NCVAS website.](http://www.va.gov/vetdata/Expenditures.asp) 
+
+This map-based exploration tool focuses exclusively on the 2015 data. 
+
+### Expenditure Data Sources and Notes
+
+The expenditure data in the GDX report are compiled from a variety of VA ogranizations/systems. The list below shows all the sources for the expediture data:
+
+1. USASpending.gov for Compensation & Pension (C&P) and Education and Vocational Rehabilitation and Employment (EVRE) Benefits.
+
+2. Veterans Benefits Administration Insurance Center for the Insurance costs.
+
+3. VA's Financial Management System (FMS) for Construction, Medical Research, General Operating Expenses, and certain C&P and Readjustment data.
+
+4. Allocation Resource Center (ARC) for Medical Care costs.
+
+5. Expenditures are rounded to the nearest thousand dollars. For example, values from $0 to $499 are rounded to $0 and values from $500 to $1,000 are rounded to $1.
+
+### Shapefile and FIPS code Source
+
+1. The 2010 State and County FIPS codes are used to create the GDX reports. [The FIPS codes were obtained from the U.S. Census Bureau website.](https://www.census.gov/geo/reference/codes/cou.html)
+
+2. The 2015 shapefile used in this application was also obtained from [the U.S. Census (TIGER) website.](https://www.census.gov/geo/maps-data/data/tiger-line.html)
+
+### Compensation & Pension Notes
+
+These expenditures include dollars for the following programs:
+
+1. Veterans' compensation for service-connected disabilities.
+    
+2. Dependency and indemnity compensation for service-connected deaths.
+    
+3. Veterans' pension for non-service-connected disabilities.
+    
+4. Burial and other benefits to Veterans and their survivors.  		
+       
+### Medical Care Notes
+
+Medical Care expenditures include dollars for medical services, medical administration, facility maintenance, educational support, research support, and other overhead items. Medical Care expenditures do not include dollars for construction or other non-medical support. Medical Care expenditures are allocated to the patient's home location, not the site of care.
+
+### Loan Guaranty Notes
+
+Currently, all "Loan Guaranty" expenditures are attributed to Travis County, TX, where all Loan Guaranty payments are processed. Consequently these expenditures are not shown in the maps. Prior to FY 08, "Loan Guaranty" expenditures were included in the Education & Vocational Rehabilitation and Employment (E&VRE) programs. VA will continue to improve data collection for future GDX reports to better distribute loan expenditures at the state, county and congressional district levels.
+
+### Veteran  Population (VetPop) Notes
+
+Veteran population estimates, as of September 30, 2015, are produced by the VA Office of the Actuary (VetPop 2014).	
+
+### Unique Patients Notes
+
+Patients who received treatment at a VA health care facility. Data are provided by the Allocation Resource Center (ARC).
+
+
+### Aknowledgements
+
+This application was built in R and RStudio and uses Leaflet - an open-source JavaScript mapping library. This tool could not have been created without the help of the people and resource listed below. You can get [the code for this application on GitHub.](https://github.com/mihiriyer/gdxleaf)
+
+1. [Leaflet for R](https://rstudio.github.io/leaflet/) - The leaflet R package provide an easy way to interface with the Leaflet. 
+
+2. [Mapping US Counties in R with FIPS](https://www.datascienceriot.com/mapping-us-counties-in-r-with-fips/kris/) - This lovely tutorial by Kris Eberwein was used to link FIPS code based data to a Census shapefile and to plot maps using ggplot2 and Leaflet. 
+
+3. [Superzip Shiny Application](http://shiny.rstudio.com/gallery/superzip-example.html) - This application was the inspiration for the look and feel. 
+
+4. [Mapping With Shapefiles in R Getting Started](http://flowingdata.com/2014/11/20/mapping-data-in-shapefile-format-with-r/) - Nathan Yau's tutorial on reading Census shapefiles does an excellent job in laying down the foundations. 
+
+5. [ColorBrewer](http://colorbrewer2.org/) - As always Cynthia Brewer saves the day in selecting color schemes. 
--- app.R
+++ app.R
@@ -1,3 +1,5 @@
+# Data Setup/Prep ---------------------------------------------------------
+
 #load the libraries
 library(shiny)
 library(dplyr)
@@ -7,6 +9,7 @@ library(rgdal)
 library(maptools)
 library(scales)
 library(leaflet)
+
 
 ###MAP Setup
 #read census shape file
@@ -19,34 +22,57 @@ us.map <- us.map[!us.map$STATEFP %in% c("78", "60", "69", "64", "68", "70", "74"
 ##DATA Setup
 #load the R data object of gdxcty - the FY10-15 consolidated, cleanedup, and linked to FIPS code
 gdxcty15 <- readRDS("gdxcty15.Rda") 
+#DIVIDE by 1000 to make numbers in MILLIONS and then round all numbers (remove decimal places)
+gdxcty15[,8:17] <- round(gdxcty15[,8:17]/1000, digits = 2)
 
 #Concatenate StateFP and CountyFP for a unique ID that can be matched with GEOID in us.map
 gdxcty15 <- mutate(gdxcty15, FIPS=paste(StateFP, CountyFP, sep="")) %>% 
   select(c(1, 18, 9, 16, 10, 12, 11, 15,14, 8, 17))
 
-
-
 #As of FY15 - Shannon County SD (Fips: 46-113) is now Ogmerge(us.map, county_dat, by.x="GEOID", by.y="FIPS")alala-Lakota County (Fips: 46-102)
 gdxcty15$FIPS[gdxcty15$FIPS == "46113"] <- "46102"
 
-#create a vector with col names to allow selecting a variable to view
+#this helper links the GDX variables to color schemes
 gdxhelper <- data.frame(
-  gdxvars= names(gdxcty15[3:11]),
-  divpals=c("BrBG", "RdBu", "PiYG", "RdGy", "RdYlBu", "PRGn", "RdYlGn","PuOr", "Spectral"),
+  gdxlabs = c("Total Expenditures",
+              "Medical Care",
+              "Compensation & Pension",
+              "Education",
+              "Construction",
+              "Insurance & Indemnities",
+              "General Operating Expenses",
+              "Veteran Popuation",
+              "Unique Patients"),
+  gdxvars = names(gdxcty15[3:11]),
+  divpals = c("BrBG", "RdBu", "PiYG", "RdGy", "RdYlBu", "PRGn", "RdYlGn","PuOr", "Spectral"),
   stringsAsFactors = FALSE)
-
-
-# c("BuGn", "YlOrRd", "BuPu", "YlGnBu", "GnBu", "PuBuGn", "OrRd", "YlOrBr", "PuBu")
-# gdxvars= names(gdxcty15[3:11])
 
 #merge the gdx data with the spatial object
 us.map <- merge(us.map, gdxcty15, by.x="GEOID", by.y="FIPS")
 rm(gdxcty15)
 
 
-#User Interface
-ui <- navbarPage("GDX 2015", theme = "bootstrap.css",
-                 tabPanel("My Application",
+#histogram theme
+hg.theme <- theme(text=element_text(color="grey25"),
+                  axis.ticks.y=element_blank(),
+                  axis.ticks.x=element_line(color="grey25"),
+                  axis.text.y=element_text(size=12, vjust=0.25),
+                  axis.text.x=element_text(size=12, hjust=0.5),
+                  axis.title.y=element_text(size=12, vjust=2.5),
+                  axis.title.x=element_text(size=12, vjust=-1.5), 
+                  panel.grid.major.y=element_line(size=0.5, linetype=3, color="grey25"),
+                  panel.grid.major.x=element_blank(),
+                  panel.grid.minor=element_blank(),
+                  panel.border=element_rect(linetype=0),
+                  panel.background=element_rect(fill="#FFFFF0"),
+                  plot.margin=unit(c(0.1, 0.15, 0.1, 0.1), "inches"),
+                  plot.title=element_text(color="black", face="bold", size=20, hjust=0, vjust=3)
+)
+
+# UI - User Interface Setup -----------------------------------------------
+
+ui <- navbarPage("VA Expenditures in Fiscal Year 2015", 
+                 tabPanel("GDX Explorer",
                           div(class="outer",
                               tags$head(
                                 # Include our custom CSS
@@ -60,44 +86,46 @@ ui <- navbarPage("GDX 2015", theme = "bootstrap.css",
                                             fixed = TRUE,
                                             draggable = TRUE,
                                             top = "auto",
-                                            left = 20,
+                                            left = 200,
                                             right = "auto",
-                                            bottom = "700",
+                                            bottom = 400,
                                             width = 330,
                                             height = "auto",
-                                            
-                                            #header title
-                                            h2("GDX explorer"),
-                                            
+                                            #HEADER title
+                                            h2("GDX Variables"),
                                             #drop-down to pick a gdx variable
                                             selectInput(inputId = "gdxvar",
                                                         label = "Select a variable to map",
                                                         choices = c("Total Expenditures" = "TotX",
                                                                     "Medical Care" = "MedCare",
-                                                                    "Comp & Pen" = "CP",
+                                                                    "Compensation & Pension" = "CP",
                                                                     "Education" = "EduVoc",
                                                                     "Construction" = "Cons",
-                                                                    "Insurance" = "InsInd",
-                                                                    "Operations" = "GOE",
+                                                                    "Insurance & Indemnities" = "InsInd",
+                                                                    "General Operating Expenses" = "GOE",
                                                                     "Veteran Popuation" = "VetPop",
-                                                                    "Uniques" = "Uniques"),
-                                                        selected = "TotX")
+                                                                    "Unique Patients" = "Uniques"),
+                                                        selected = "TotX"),
+                                            plotOutput(outputId = "histo", height = 200)
                               )
                           )
                           
                  ),
-                 tabPanel("Tab 01"),
-                 tabPanel("Tab 02")
+                 tabPanel("About", 
+                          div(class="abouttext",
+                              includeMarkdown("about.md")) 
+                              ),
+                 tabPanel("Code")
 )
 
+# SERVER logic ------------------------------------------------------------
 
-#SERVER logic
 server <- function(input, output){
 
   # Format popup data for leaflet map.
   popup_dat <- reactive({
     paste0("<strong>County: </strong>",
-           us.map$NAME,
+           paste(us.map$NAME, ", ", us.map$State, sep=""),
            "<br><strong>Total: </strong>",
            trimws(format(round(us.map$TotX, digits = 0), big.mark = ",")),
            "<br><strong>Medical Care: </strong>",
@@ -112,7 +140,7 @@ server <- function(input, output){
            trimws(format(round(us.map$InsInd, digits = 0), big.mark = ",")),
            "<br><strong>Operations: </strong>",
            trimws(format(round(us.map$GOE, digits = 0), big.mark = ",")),
-           "<br><strong>VetPop: </strong>",
+           "<br><strong>Veteran Population: </strong>",
            trimws(format(round(us.map$VetPop, digits = 0), big.mark = ",")),
            "<br><strong>Uniques: </strong>",
            trimws(format(round(us.map$Uniques, digits = 0), big.mark = ","))
@@ -120,34 +148,66 @@ server <- function(input, output){
     })
   
   #create a color palette
-  pal <- reactive({
-    colorQuantile(gdxhelper$divpals[gdxhelper$gdxvars == input$gdxvar],
-                  domain = as.numeric(us.map@data[input$gdxvar][,1]),
-                  n = 7)
-  })
-
   # pal <- reactive({
-  #   colorBin(gdxhelper$divpals[gdxvars == input$gdxvar], 
+  #   colorQuantile(gdxhelper$divpals[gdxhelper$gdxvars == input$gdxvar],
+  #                 domain = as.numeric(us.map@data[input$gdxvar][,1]),
+  #                 n = 7)
+  # })
+  
+  # pal <- reactive({
+  #   colorBin(gdxhelper$divpals[gdxhelper$gdxvars == input$gdxvar],
   #            domain = as.numeric(us.map@data[input$gdxvar][,1]),
-  #            bins = 7
+  #            bins = 20
   #            )
   # })
-    
-  #and zee eaflet
+  
+  pal <- reactive({
+    colorNumeric(gdxhelper$divpals[gdxhelper$gdxvars == input$gdxvar],
+             domain = as.numeric(us.map@data[input$gdxvar][,1])
+    )
+  })
+  
+  xlabel <- reactive(paste(gdxhelper$gdxlabs[gdxhelper$gdxvars == input$gdxvar], "(in 000s)"))
+
+
+  #and zee leaflet
   output$mymap <- renderLeaflet({
-        leaflet(data = us.map)  %>%
+    leaflet(data = us.map)  %>%
       addTiles() %>%
       addPolygons(fillColor = ~pal()(as.numeric(us.map@data[input$gdxvar][,1])), 
                   fillOpacity = 0.8, 
                   color = "#BDBDC3", 
                   weight = 1,
                   popup = popup_dat()) %>% 
-      addLegend("bottomright", pal = pal(), values = ~as.numeric(us.map@data[input$gdxvar][,1]),
-                title = input$gdxvar,
+      addLegend("bottomright", 
+                pal = pal(), 
+                values = ~as.numeric(us.map@data[input$gdxvar][,1]),
+                title = xlabel(),
                 labFormat = labelFormat(prefix = ""),
                 opacity = 1) %>% 
-      setView(lng = -93.85, lat = 37.45, zoom = 4)
+      setView(lng = -110.00, lat = 45.00, zoom = 4)
   })
+  
+  #und ein histogramsicle 
+  output$histo <- renderPlot(
+    ggplot2::qplot(x = as.numeric(us.map@data[input$gdxvar][,1]),
+                   geom = "histogram",
+                   bins=15, 
+                   main = NULL,
+                   xlab = "mnky"
+    ) +
+      #custom y-axis
+      scale_y_continuous(name = "No. of Counties (in 00s)",
+                         breaks = seq(0, 3200, 600),
+                         labels = seq(0, 32, 6),
+                         limits = c(0,3220)) +
+      #apply blank theme
+      theme_bw() +
+      #apply custom theme
+      hg.theme
+    )
+  
+  
 }
 
 shinyApp(ui = ui, server = server)
